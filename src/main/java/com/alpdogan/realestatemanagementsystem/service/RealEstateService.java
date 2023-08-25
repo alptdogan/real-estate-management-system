a63433b14b59b89143e2b63ab5bb68d52656ee8b
alptdogan <alpdogan000@gmail.com>
2023-08-25 13:35:51 +0300

    Add model mapper dependency and refactor the getAll method to return data through a response dto

--- src/main/java/com/alpdogan/realestatemanagementsystem/service/RealEstateService.java
+++ src/main/java/com/alpdogan/realestatemanagementsystem/service/RealEstateService.java
@@ -1,10 +1,13 @@
 package com.alpdogan.realestatemanagementsystem.service;
 
 import com.alpdogan.realestatemanagementsystem.dto.request.SaveRealEstateRequestDto;
+import com.alpdogan.realestatemanagementsystem.dto.response.RealEstateResponseDto;
 import com.alpdogan.realestatemanagementsystem.entity.RealEstate;
 import com.alpdogan.realestatemanagementsystem.repository.RealEstateRepository;
+import org.modelmapper.ModelMapper;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
@@ -12,9 +15,11 @@ import java.util.Optional;
 public class RealEstateService {
 
     private RealEstateRepository realEstateRepository;
+    private ModelMapper modelMapper;
 
-    public RealEstateService(RealEstateRepository realEstateRepository) {
+    public RealEstateService(RealEstateRepository realEstateRepository, ModelMapper modelMapper) {
         this.realEstateRepository = realEstateRepository;
+        this.modelMapper = modelMapper;
     }
 
     public RealEstate saveRealEstate(SaveRealEstateRequestDto saveRealEstateRequestDto){
@@ -38,8 +43,18 @@ public class RealEstateService {
         return realEstateRepository.findById(realEstateId);
     }
 
-    public List<RealEstate> getAllRealEstates() {
-        return realEstateRepository.findAll();
+    public List<RealEstateResponseDto> getAllRealEstates() {
+
+        Iterable<RealEstate> realEstates = realEstateRepository.findAll();
+        List<RealEstateResponseDto> realEstateResponseDtos = new ArrayList<>();
+
+        for(RealEstate realEstate : realEstates) {
+            RealEstateResponseDto realEstateResponseDto = modelMapper.map(realEstate, RealEstateResponseDto.class);
+            realEstateResponseDtos.add(realEstateResponseDto);
+        }
+
+        return realEstateResponseDtos;
+
     }
 
     public void deleteRealEstateById(int realEstateId) {
