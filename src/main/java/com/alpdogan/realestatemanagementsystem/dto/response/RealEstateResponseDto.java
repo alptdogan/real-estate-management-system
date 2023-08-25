44a4651b404b0f5b5b34776f0304acc98cd5b8d2
alptdogan <alpdogan000@gmail.com>
2023-08-25 13:36:35 +0300

    Add id as a field to return ids also

--- src/main/java/com/alpdogan/realestatemanagementsystem/dto/response/RealEstateResponseDto.java
+++ src/main/java/com/alpdogan/realestatemanagementsystem/dto/response/RealEstateResponseDto.java
@@ -7,6 +7,7 @@ import javax.persistence.Column;
 @Data
 public class RealEstateResponseDto {
 
+    private int realEstateId;
     private String realEstateName;
     private int squareMeters;
     private int numberOfRooms;
