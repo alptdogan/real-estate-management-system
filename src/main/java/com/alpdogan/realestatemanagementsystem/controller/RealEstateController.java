e2cacecb6e53bb9b189d53c1245b6737b3a71c5b
alptdogan <alpdogan000@gmail.com>
2023-08-25 12:53:17 +0300

    Add getById and getAll methods

--- src/main/java/com/alpdogan/realestatemanagementsystem/controller/RealEstateController.java
+++ src/main/java/com/alpdogan/realestatemanagementsystem/controller/RealEstateController.java
@@ -2,10 +2,10 @@ package com.alpdogan.realestatemanagementsystem.controller;
 
 import com.alpdogan.realestatemanagementsystem.entity.RealEstate;
 import com.alpdogan.realestatemanagementsystem.service.RealEstateService;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/real_estates")
@@ -20,6 +20,16 @@ public class RealEstateController {
     @PostMapping("/addRealEstate")
     public RealEstate addRealEstate(@RequestBody RealEstate realEstate) {
         return realEstateService.saveRealEstate(realEstate);
+    }
+
+    @GetMapping("{realEstateId}")
+    public Optional<RealEstate> getRealEstateById(@PathVariable int realEstateId) {
+        return realEstateService.getRealEstateById(realEstateId);
+    }
+
+    @GetMapping
+    public List<RealEstate> getAllRealEstates() {
+        return realEstateService.getAllRealEstates();
     }
 
 }
--- src/main/java/com/alpdogan/realestatemanagementsystem/service/RealEstateService.java
+++ src/main/java/com/alpdogan/realestatemanagementsystem/service/RealEstateService.java
@@ -4,6 +4,9 @@ import com.alpdogan.realestatemanagementsystem.entity.RealEstate;
 import com.alpdogan.realestatemanagementsystem.repository.RealEstateRepository;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+import java.util.Optional;
+
 @Service
 public class RealEstateService {
 
@@ -15,6 +18,14 @@ public class RealEstateService {
 
     public RealEstate saveRealEstate(RealEstate realEstate){
         return realEstateRepository.save(realEstate);
+    }
+
+    public Optional<RealEstate> getRealEstateById(int realEstateId) {
+        return realEstateRepository.findById(realEstateId);
+    }
+
+    public List<RealEstate> getAllRealEstates() {
+        return realEstateRepository.findAll();
     }
 
 }
