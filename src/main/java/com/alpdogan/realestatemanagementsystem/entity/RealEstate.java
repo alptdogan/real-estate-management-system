31516208ed05c472f20c828304533b3cbc8a0cda
alptdogan <alpdogan000@gmail.com>
2023-08-25 12:13:22 +0300

    Create RealEstate entity with some of its fields

---
+++ src/main/java/com/alpdogan/realestatemanagementsystem/entity/RealEstate.java
@@ -0,0 +1,31 @@
+package com.alpdogan.realestatemanagementsystem.entity;
+
+import lombok.Data;
+
+import javax.persistence.*;
+
+@Data
+@Entity
+@Table(name = "real_estate")
+public class RealEstate {
+
+    //buraya gerektikçe yeni fieldlar gelecek. emlak türünü enum yapabilirim.
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column (name = "id")
+    private int id;
+
+    @Column(name = "real_estate_name")
+    private String realEstateName;
+
+    @Column(name = "square_meters")
+    private int squareMeters;
+
+    @Column(name = "number_of_rooms")
+    private int numberOfRooms;
+
+    @Column(name = "floor_no")
+    private int floorNo;
+
+}
