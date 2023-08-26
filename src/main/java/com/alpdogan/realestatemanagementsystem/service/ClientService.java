c30278f02c79eb2736c7f8883a933c9cf613bb10
alptdogan <alpdogan000@gmail.com>
2023-08-26 12:12:15 +0300

    Update, refactor for real estate relation

--- src/main/java/com/alpdogan/realestatemanagementsystem/service/ClientService.java
+++ src/main/java/com/alpdogan/realestatemanagementsystem/service/ClientService.java
@@ -2,16 +2,22 @@ package com.alpdogan.realestatemanagementsystem.service;
 
 import com.alpdogan.realestatemanagementsystem.dto.request.SaveClientRequestDto;
 import com.alpdogan.realestatemanagementsystem.entity.Client;
+import com.alpdogan.realestatemanagementsystem.entity.RealEstate;
 import com.alpdogan.realestatemanagementsystem.repository.ClientRepository;
+import com.alpdogan.realestatemanagementsystem.repository.RealEstateRepository;
 import org.springframework.stereotype.Service;
+
+import java.util.Collections;
 
 @Service
 public class ClientService {
 
     private ClientRepository clientRepository;
+    private RealEstateRepository realEstateRepository;
 
-    public ClientService(ClientRepository clientRepository) {
+    public ClientService(ClientRepository clientRepository, RealEstateRepository realEstateRepository) {
         this.clientRepository = clientRepository;
+        this.realEstateRepository = realEstateRepository;
     }
 
     public Client saveClient(SaveClientRequestDto saveClientRequestDto) {
@@ -20,13 +26,16 @@ public class ClientService {
         String lastNameRequest = saveClientRequestDto.getLastName();
         Long phoneNumberRequest = saveClientRequestDto.getPhoneNumber();
         String emailAddressRequest = saveClientRequestDto.getEmailAddress();
+        int realEstateIdRequest = saveClientRequestDto.getRealEstateId();
 
+        RealEstate realEstate = realEstateRepository.findById(realEstateIdRequest).get();
         Client client = new Client();
 
         client.setFirstName(firstNameRequest);
         client.setLastName(lastNameRequest);
         client.setPhoneNumber(phoneNumberRequest);
         client.setEmailAddress(emailAddressRequest);
+        client.setRealEstates(Collections.singletonList(realEstate));
 
         return clientRepository.save(client);
 
