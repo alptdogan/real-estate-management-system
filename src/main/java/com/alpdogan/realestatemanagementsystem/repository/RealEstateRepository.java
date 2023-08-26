b6e58ee6ae9d21c6d697fda17205ce7ac1826814
alptdogan <alpdogan000@gmail.com>
2023-08-26 18:38:27 +0300

    Add query to find real estates based on their number of rooms

--- src/main/java/com/alpdogan/realestatemanagementsystem/repository/RealEstateRepository.java
+++ src/main/java/com/alpdogan/realestatemanagementsystem/repository/RealEstateRepository.java
@@ -2,6 +2,14 @@ package com.alpdogan.realestatemanagementsystem.repository;
 
 import com.alpdogan.realestatemanagementsystem.entity.RealEstate;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
 
 public interface RealEstateRepository extends JpaRepository<RealEstate, Integer> {
+
+    @Query("SELECT re FROM RealEstate re WHERE re.numberOfRooms = :numberOfRooms")
+    List<RealEstate> searchByNumberOfRooms(@Param("numberOfRooms") int numberOfRooms);
+
 }
