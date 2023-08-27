324c6498c9061601205fa32015503131eb0cf47a
alptdogan <alpdogan000@gmail.com>
2023-08-27 16:32:36 +0300

    Create a basic security configuration class for now

---
+++ src/main/java/com/alpdogan/realestatemanagementsystem/configuration/SecurityConfiguration.java
@@ -0,0 +1,61 @@
+package com.alpdogan.realestatemanagementsystem.configuration;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+
+import static org.springframework.security.config.Customizer.withDefaults;
+
+@Configuration
+@EnableWebSecurity
+@EnableGlobalMethodSecurity(prePostEnabled = true)
+@RequiredArgsConstructor
+public class SecurityConfiguration {
+
+    @Bean
+    public InMemoryUserDetailsManager userDetailsService() {
+
+        UserDetails client = User.withDefaultPasswordEncoder()
+                .username("client")
+                .password("client")
+                .roles("CLIENT")
+                .build();
+        UserDetails admin = User.withDefaultPasswordEncoder()
+                .username("admin")
+                .password("admin")
+                .roles("ADMIN")
+                .build();
+        return new InMemoryUserDetailsManager(client, admin);
+
+    }
+
+    // home, login ve dashboard sayfaları oluşturulacak. sonrasında uzantılara kısıtlamalar atayacağım.
+    @Bean
+    public SecurityFilterChain configure(HttpSecurity http) throws Exception {
+
+        return http
+                .csrf(csrf -> csrf.disable())
+                .authorizeRequests(auth -> {
+                    try {
+                        auth.antMatchers("/home").permitAll()
+                                .antMatchers("/dashboard").authenticated()
+                                .and().formLogin().loginPage("/login")
+                                .defaultSuccessUrl("/dashboard").failureUrl("/login?error=true").permitAll()
+                                .and().logout().logoutSuccessUrl("/login?logout=true").invalidateHttpSession(true).permitAll();
+                    } catch (Exception e) {
+                        throw new RuntimeException(e);
+                    }
+                })
+                .httpBasic(withDefaults())
+                .build();
+
+    }
+
+}
