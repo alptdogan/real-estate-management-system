71fc1dc992c35998ff2a31dd4a5e74a646c9bfdd
alptdogan <alpdogan000@gmail.com>
2023-08-27 13:34:11 +0300

    Add queries to find real estates by their floor numbers, square meters and types one by one

--- src/main/java/com/alpdogan/realestatemanagementsystem/repository/RealEstateRepository.java
+++ src/main/java/com/alpdogan/realestatemanagementsystem/repository/RealEstateRepository.java
@@ -1,5 +1,7 @@
 package com.alpdogan.realestatemanagementsystem.repository;
 
+import com.alpdogan.realestatemanagementsystem.dto.response.TypeRealEstateResponseDto;
+import com.alpdogan.realestatemanagementsystem.entity.ERealEstateType;
 import com.alpdogan.realestatemanagementsystem.entity.RealEstate;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
@@ -11,5 +13,17 @@ public interface RealEstateRepository extends JpaRepository<RealEstate, Integer>
 
     @Query("SELECT re FROM RealEstate re WHERE re.numberOfRooms = :numberOfRooms")
     List<RealEstate> searchByNumberOfRooms(@Param("numberOfRooms") int numberOfRooms);
+
+    @Query("SELECT re FROM RealEstate re WHERE re.squareMeters = :squareMeters")
+    List<RealEstate> searchBySquareMeters(@Param("squareMeters") int squareMeters);
+
+    @Query("SELECT re FROM RealEstate re WHERE re.floorNo = :floorNo")
+    List<RealEstate> searchByFloorNo(@Param("floorNo") int floorNo);
+
+//    @Query("SELECT re FROM RealEstate re WHERE re.eRealEstateType = :eRealEstateType")
+//    List<RealEstate> searchByRealEstateType();
+
+    @Query("SELECT re FROM RealEstate re WHERE re.eRealEstateType = :eRealEstateType")
+    List<RealEstate> searchByRealEstateType(@Param("eRealEstateType") ERealEstateType eRealEstateType);
 
 }
