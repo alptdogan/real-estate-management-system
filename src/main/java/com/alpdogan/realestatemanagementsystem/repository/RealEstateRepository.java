43bc85c5bd63ac99c3bb1ae603174a6b8795694d
alptdogan <alpdogan000@gmail.com>
2023-08-28 17:11:32 +0300

    Add new queries for the new fields, for clients to search

--- src/main/java/com/alpdogan/realestatemanagementsystem/repository/RealEstateRepository.java
+++ src/main/java/com/alpdogan/realestatemanagementsystem/repository/RealEstateRepository.java
@@ -1,6 +1,8 @@
 package com.alpdogan.realestatemanagementsystem.repository;
 
 import com.alpdogan.realestatemanagementsystem.entity.ERealEstateType;
+import com.alpdogan.realestatemanagementsystem.entity.ESaleOrRent;
+import com.alpdogan.realestatemanagementsystem.entity.ETown;
 import com.alpdogan.realestatemanagementsystem.entity.RealEstate;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
@@ -19,18 +21,32 @@ public interface RealEstateRepository extends JpaRepository<RealEstate, Integer>
     @Query("SELECT re FROM RealEstate re WHERE re.floorNo = :floorNo")
     List<RealEstate> searchByFloorNo(@Param("floorNo") int floorNo);
 
+    @Query("SELECT re FROM RealEstate re WHERE re.price = :price")
+    List<RealEstate> searchByPrice(@Param("price") int price);
+
     @Query("SELECT re FROM RealEstate re WHERE re.eRealEstateType = :eRealEstateType")
     List<RealEstate> searchByRealEstateType(@Param("eRealEstateType") ERealEstateType eRealEstateType);
+
+    @Query("SELECT re FROM RealEstate re WHERE re.eSaleOrRent = :eSaleOrRent")
+    List<RealEstate> searchBySaleOrRent(@Param("eSaleOrRent") ESaleOrRent eSaleOrRent);
+
+    @Query("SELECT re FROM RealEstate re WHERE re.eTown = :eTown")
+    List<RealEstate> searchByTown(@Param("eTown") ETown eTown);
+
 
     @Query("SELECT re FROM RealEstate re " +
             "WHERE re.numberOfRooms = :numberOfRooms " +
             "AND re.floorNo = :floorNo " +
             "AND re.squareMeters = :squareMeters " +
-            "AND re.eRealEstateType = :eRealEstateType")
+            "AND re.eRealEstateType = :eRealEstateType " +
+            "AND re.eSaleOrRent = :eSaleOrRent " +
+            "AND re.eTown = :eTown" )
     List<RealEstate> findRealEstatesByItsProperties(@Param("numberOfRooms") int numberOfRooms,
-                                                @Param("floorNo") int floorNo,
-                                                @Param("squareMeters") int squareMeters,
-                                                @Param("eRealEstateType") ERealEstateType realEstateType);
+                                                    @Param("floorNo") int floorNo,
+                                                    @Param("squareMeters") int squareMeters,
+                                                    @Param("eRealEstateType") ERealEstateType realEstateType,
+                                                    @Param("eSaleOrRent") ESaleOrRent saleOrRent,
+                                                    @Param("eTown") ETown town);
 
 }
 
