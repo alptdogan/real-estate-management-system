c00dabc77f000ca3034564af405ad55c25e7927b
alptdogan <alpdogan000@gmail.com>
2023-08-28 17:09:49 +0300

    Add new fields and rename the field "realEstateName" to "realEstateAddress"

--- src/main/java/com/alpdogan/realestatemanagementsystem/entity/RealEstate.java
+++ src/main/java/com/alpdogan/realestatemanagementsystem/entity/RealEstate.java
@@ -6,7 +6,7 @@ import javax.persistence.*;
 
 @Data
 @Entity
-@Table(name = "real_estate")
+@Table(name = "real_estates")
 public class RealEstate {
 
     @Id
@@ -14,8 +14,11 @@ public class RealEstate {
     @Column (name = "real_estate_id")
     private int realEstateId;
 
-    @Column(name = "real_estate_name")
-    private String realEstateName;
+    @Column(name = "advert_title")
+    private String advertTitle;
+
+    @Column(name = "real_estate_address")
+    private String realEstateAddress;
 
     @Column(name = "square_meters")
     private int squareMeters;
@@ -26,6 +29,9 @@ public class RealEstate {
     @Column(name = "floor_no")
     private int floorNo;
 
+    @Column(name = "price")
+    private int price;
+
     @ManyToOne
     private Client client;
 
@@ -33,5 +39,12 @@ public class RealEstate {
     @Column(name = "real_estate_type")
     private ERealEstateType eRealEstateType;
 
+    @Enumerated(EnumType.STRING)
+    @Column(name = "sale_or_rent")
+    private ESaleOrRent eSaleOrRent;
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "town")
+    private ETown eTown;
 
 }
