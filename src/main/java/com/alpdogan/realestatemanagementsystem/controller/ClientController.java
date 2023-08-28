097ceea06d25ab3b0b44403c5b9c52b9b2e371c1
alptdogan <alpdogan000@gmail.com>
2023-08-28 21:46:06 +0300

    Add /details extension for details page, and also make all methods to return an html file

--- src/main/java/com/alpdogan/realestatemanagementsystem/controller/ClientController.java
+++ src/main/java/com/alpdogan/realestatemanagementsystem/controller/ClientController.java
@@ -4,12 +4,14 @@ import com.alpdogan.realestatemanagementsystem.dto.request.SaveClientRequestDto;
 import com.alpdogan.realestatemanagementsystem.dto.response.ClientResponseDto;
 import com.alpdogan.realestatemanagementsystem.entity.Client;
 import com.alpdogan.realestatemanagementsystem.service.ClientService;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 import java.util.Optional;
 
-@RestController
+@Controller
 @RequestMapping("/clients")
 public class ClientController {
 
@@ -20,9 +22,23 @@ public class ClientController {
     }
 
     @PostMapping("/addClient")
-    public Client addClient(@RequestBody SaveClientRequestDto saveClientRequestDto) {
-        return clientService.saveClient(saveClientRequestDto);
+    public String addClient(@ModelAttribute("client") SaveClientRequestDto saveClientRequestDto) {
+
+        clientService.saveClient(saveClientRequestDto);
+
+        return "redirect:/clients";
+
     }
+    @GetMapping("/new")
+    public String displayClientForm (Model model) {
+
+        SaveClientRequestDto saveClientRequestDto = new SaveClientRequestDto();
+        model.addAttribute("client", saveClientRequestDto);
+
+        return "client-add";
+
+    }
+
 
     @GetMapping("/{clientId}")
     public Optional<Client> getClientById(@PathVariable int clientId) {
@@ -30,13 +46,31 @@ public class ClientController {
     }
 
     @GetMapping
-    public List<ClientResponseDto> getAllClients() {
-        return clientService.getAllClients();
+    public String getAllClients(Model model) {
+
+        List<ClientResponseDto> clientResponseDtos = clientService.getAllClients();
+        model.addAttribute("clients", clientResponseDtos);
+
+        return "clients-list";
     }
 
-    @DeleteMapping("/{clientId}")
-    public void deleteClientById(@PathVariable int clientId) {
+    @DeleteMapping("/deleteClientById")
+    public String deleteClientById(@RequestParam("clientId") int clientId) {
+
         clientService.deleteClientById(clientId);
+
+        return "redirect:/clients";
+
+    }
+
+    @GetMapping("/details")
+    public String displayClientDetails(@RequestParam("clientId") int clientId, Model model) {
+
+        Optional<Client> client = clientService.getClientById(clientId);
+        model.addAttribute("client", client);
+
+        return "client-details";
+
     }
 
 }
